23fc5ac962ea33e34f7e477b58022afbb3ea3424
Nils Olav Handegard <nils.olav.handegard@imr.no>
2024-11-19 10:41:13 +0100

    Create generate_report.py

---
+++ generate_report.py
@@ -0,0 +1,13 @@
+# Code that does the report generation
+
+def generate_report(report, sv, bottom, label):
+  # Do da thing
+  # report: Full path to report (export)
+  # sv :  Full path to sv zarr
+  # bottom: Full path to Bottom zarr
+  # label : Full path to Label or prediction zarr
+
+  # Save report zarr
+  # Save csv file
+
+
