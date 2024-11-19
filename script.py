f5123f14426a38414256082b10991fd8a4002edc
Nils Olav Handegard <nils.olav.handegard@imr.no>
2024-11-19 10:42:58 +0100

    Create script.py

---
+++ script.py
@@ -0,0 +1,11 @@
+# Script to run the sctipt
+
+# Read data sets
+
+# Loop over data sets
+
+for _surve in survey:
+  # report = (xarray object)
+  # sv =  (xarray object)
+  
+  generate_report(report, sv, bottom, label):
