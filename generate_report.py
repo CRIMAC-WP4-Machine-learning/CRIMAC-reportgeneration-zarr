13e1d37fa1551c4978b7daaaf628db1c96183669
Nils Olav Handegard <nils.olav.handegard@imr.no>
2024-11-19 10:43:43 +0100

    Update generate_report.py

--- generate_report.py
+++ generate_report.py
@@ -3,9 +3,9 @@
 def generate_report(report, sv, bottom, label):
   # Do da thing
   # report: Full path to report (export)
-  # sv :  Full path to sv zarr
-  # bottom: Full path to Bottom zarr
-  # label : Full path to Label or prediction zarr
+  # sv : Xarray object to sv zarr
+  # bottom:  Xarray object to Bottom zarr
+  # label :  Xarray object to Label or prediction zarr
 
   # Save report zarr
   # Save csv file
