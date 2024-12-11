801002d0f590649919a2d29237c2df47f01ea7f7
Nils Olav Handegard <nilsolav@imr.no>
2024-12-11 14:47:30 +0100

    Updated script to read zarr files and provide error message.

--- script.py
+++ script.py
@@ -1,4 +1,6 @@
 import os
+import xarray as xr
+
 # Script to generate the reports from the sv+label for the Sand eel series
 
 #crimacscratch = os.getenv('CRIMACSCRATCH')
@@ -18,7 +20,8 @@ pr = [['labels.zarr', 'report_1.zarr'],
       ['predictions_3.zarr', 'report_3.zarr'],
       ['predictions_4.zarr', 'report_4.zarr']]
 
-# This function takes the staging data over the production data
+
+# This function prioritizes the staging data over the production data
 def prodstage(crimacscratch,_cs,zarrstore):
   prod= os.path.join(crimacscratch, _cs[0:4], 'S'+_cs, zarrstore)
   staging = os.path.join(crimacscratch, 'staging', _cs[0:4],
@@ -31,15 +34,35 @@ def prodstage(crimacscratch,_cs,zarrstore):
     d = None
   return d
 
+
+def runcruise(cruise):
+  try:
+    sv_zarr = xr.open_zarr(cruise['sv_file'])
+    label_zarr = xr.open_zarr(cruise['pred_file'])
+    bottom_zarr = xr.open_zarr(cruise['bottom_file'])
+    print(sv_zarr)
+    print(label_zarr)
+    print(bottom_zarr)
+    print('Export results to ',cruise['report_file'])
+  except:
+    print(cruise)
+    print('Failed!!!!')
+
+
 def runcruises(files):
   for _files in files:
     sv_exist = files[_files]['sv_file']!=None
     bottom_exist = files[_files]['bottom_file']!=None
     pred_exist = files[_files]['pred_file']!=None
     if sv_exist & bottom_exist & pred_exist:
-      print ('Missing data for ', files[_files]['report_file'])
-             
-      print (_files, ' is missing data.)
+      #print ('Existing data for ', files[_files]['report_file'])
+      # Run processing
+      runcruise(files[_files])
+    else:
+      print ('Missing data for  ', files[_files]['report_file'])
+      #print ('sv data     : ', str(sv_exist))
+      #print ('Bottom data : ', str(bottom_exist))
+      #print ('Pred data   : ', str(pred_exist))
 
 
 files = {}
@@ -60,7 +83,23 @@ for _cs in cs:
     # Report
     _report_file = os.path.join('ACOUSTIC','REPORTS','S'+_cs+'_'+_pr[1])
     report_file = os.path.join(dataout, _cs[0:4], 'S'+_cs,_report_file)
-  files['S'+_cs] = ({'sv_file':sv_file, 'bottom_file':bottom_file,
-                'pred_file':pred_file, 'report_file':report_file})
+    files['S'+_cs+'_'+_pr[1]] = ({'sv_file':sv_file, 'bottom_file':bottom_file,
+                       'pred_file':pred_file, 'report_file':report_file})
 
 runcruises(files)
+
+'''
+os.path.exists('/data/crimac-scratch/staging/2022/S2022206/ACOUSTIC/PREDICTIONS/S2022206_predictions_4.zarr')
+nils = xr.open_zarr('/data/crimac-scratch/staging/2022/S2022206/ACOUSTIC/PREDICTIONS/S2022206_predictions_4.zarr')
+
+os.path.exists('/data/crimac-scratch/staging/2021/S2021847/ACOUSTIC/PREDICTIONS/S2021847_predictions_4.zarr')
+nils = xr.open_zarr('/data/crimac-scratch/staging/2021/S2021847/ACOUSTIC/PREDICTIONS/S2021847_predictions_4.zarr')
+
+
+\\ces.hi.no\crimac-scratch\staging\2021\S2021847\ACOUSTIC\PREDICTIONS\S2021847_predictions_4.zarr
+
+import zarr
+store = zarr.open('/data/crimac-scratch/staging/2022/S2022206/ACOUSTIC/PREDICTIONS/S2022206_predictions_4.zarr', mode='r')
+print('.zmetadata' in store)
+
+'''
